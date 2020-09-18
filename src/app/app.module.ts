15ed0c35b477770edf53a0dee11c34dd724f3c2b
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-09-18 14:20:19 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
