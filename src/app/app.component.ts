b23eca444bc04390911223b524627f6a01a35605
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-09-18 17:33:38 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'complete-guide';
+  loaderFeature = 'recipe'
+
+  onNavigate(element: string){
+    this.loaderFeature = element
+
+  }
 }
