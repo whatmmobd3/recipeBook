3a8eb8a7a781ed7e7cf1d0ef77e6afd5e2ca7211
quangloc.nguyen <you@example.com>
2020-10-01 14:12:59 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  loaderFeature = 'recipe'
+  loaderFeature = 'recipe';
 
-  onNavigate(element: string){
-    this.loaderFeature = element
-
+  onNavigate(element: string) {
+    this.loaderFeature = element;
   }
 }
